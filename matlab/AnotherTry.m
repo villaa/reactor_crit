b745654dbd9b3d31efc1aa26add946250be17505
rebekahmoline <47698863+rebekahmoline@users.noreply.github.com>
2019-02-24 16:19:21 -0700

    @ 4:15 on 2/24/19

--- matlab/AnotherTry.m
+++ matlab/AnotherTry.m
@@ -5,22 +5,28 @@ g = input('Enter number of generations = ');
 n = ni; %initially set total neutrons to initial number inputted
 t=0;
 out = zeros(g,2);
-out(1,1)= n;
-out(1,2)= t;
+out(1,1)= t;
+out(1,2)= n;
 for z=1:g
     for a=1:n
-        x=rand();
-        y=rand();
+    x=rand();
+    y=rand();
+    t = y * 200;
         if (0.81 < x)    %if random # is greater than 0.81 it leaked
-            n = n - 1 ;   %If leaked total nuetrons-1
+            deltan = -1 ;   %If leaked total nuetrons-1
+            n = n + deltan;
         end
-        t = y * 200;
-    end
-         out(z+1,1)=n;
-         out(z+1,2)=t;
-         p = plot(t,n);
-         csvwrite('NofTData.csv', [n; t]);
+        out(a,2)=n;
+        out(a,1)=t;
+    end               
 end
-
-out
+%csvwrite('NofTData.csv', [n+deltan; t]);
+B = sortrows (out, 1);
+figure;
+plot(B,':+b');
+title('Neutrons over Time')
+xlabel('Time in microseconds')
+ylabel('Neutrons')
+%xlswrite('NofTxcel.xlsx',B)
+B
 toc;
