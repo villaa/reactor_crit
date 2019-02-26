93354184bba2b4af66c8ac5442701b3837c63983
rebekahmoline <47698863+rebekahmoline@users.noreply.github.com>
2019-02-26 13:15:13 -0700

    Meeting

--- matlab/AnotherTry.m
+++ matlab/AnotherTry.m
@@ -5,39 +5,42 @@ g = input('Enter number of generations = ');
 nt = ni; %initially set total neutrons to initial number inputted
 t=0;
 dn=0;
-out = zeros((g*nt),2);
-out(1,1)= t;
-out(1,2)= dn;
+%out(1,1)= t;
+%out(1,2)= dn;
+out=zeros(0,2);
 for z=1:g
     for a=1:nt
     x=rand();
     y=rand();
         if (0.81 < x)    %if random # is greater than 0.81 it leaked
             dn = -1 ;   %If leaked total nuetrons-1
-            nt=nt+dn;
+            nt = nt+dn;
             t = y * 200;
+            out=[out;[t,dn]];
         end
-        out(a,2)=dn;
-        out(a,1)=t;
-    end               
+        %out(a,2)=dn;
+        %out(a,1)=t;
+        
+    end     
+    if nt==0 break;
+    end
 end
-B = sortrows (out, 1); %sort numerically by column one time
-out=B;                      %throw out any 0 data
-out(any(B==0,2),:) = [];    
-A= ni- B;       %I want to preform an operation on column 2 of B (ni-dn)
-                                %and have this column be the new 2nd culomn in matrix A
+B = sortrows (out, 1); %sort numerically by column one time   
+%out(any(B==0,2),:) = [];  
+B= [[0,ni];B];
+B(:,2)=cumsum(B(:,2),1);     
 %C= (B(:1)),(A(:2));
-C = B(:, 1);
-C = A(:, 2);
+%C = B(:, 1);
+%C = A(:, 2);
 fileID = fopen('NofTData.txt','w');%write a a text file
-fprintf(fileID,'%3f %i\n',B); %3digits past decimal in time integer for n total
+fprintf(fileID,'%3f %i\n',C'); %3digits past decimal in time integer for n total
 fclose(fileID); 
-figure;
-T=B(:,1);
-N=A(:,2);
-plot(T,N);
-title('Neutrons over Time')
-xlabel('Time in microseconds')
-ylabel('Neutrons')
-C
+%figure;
+%T=B(:,1);
+%N=A(:,2);
+%plot(T,N);
+%title('Neutrons over Time')
+%xlabel('Time in microseconds')
+%ylabel('Neutrons')
+B
 toc;
