14e2549601b3bd74280854d8bf2712fcb4120e3d
rebekahmoline <47698863+rebekahmoline@users.noreply.github.com>
2019-02-28 19:30:13 -0700

    I have changed the code to calculate an extra 200 microseconds onto each consecutive generation and have the beginnings of the header line however am having issues finding the git code version and how matlab can call that to print it to the header

--- matlab/AnotherTry.m
+++ matlab/AnotherTry.m
@@ -5,8 +5,6 @@ g = input('Enter number of generations = ');
 nt = ni; %initially set total neutrons to initial number inputted
 t=0;
 dn=0;
-%out(1,1)= t;
-%out(1,2)= dn;
 out=zeros(0,2);
 for z=1:g
     for a=1:nt
@@ -15,32 +13,21 @@ for z=1:g
         if (0.81 < x)    %if random # is greater than 0.81 it leaked
             dn = -1 ;   %If leaked total nuetrons-1
             nt = nt+dn;
-            t = y * 200;
+            %t = y * 200;
+            t = (y*200) +(z*200);
             out=[out;[t,dn]];
-        end
-        %out(a,2)=dn;
-        %out(a,1)=t;
-        
+        end    
     end     
     if nt==0 break;
     end
 end
-B = sortrows (out, 1); %sort numerically by column one time   
-%out(any(B==0,2),:) = [];  
+B = sortrows (out, 1); %sort numerically by column one time     
 B= [[0,ni];B];
 B(:,2)=cumsum(B(:,2),1);     
-%C= (B(:1)),(A(:2));
-%C = B(:, 1);
-%C = A(:, 2);
 fileID = fopen('NofTData.txt','w');%write a a text file
-fprintf(fileID,'%3f %i\n',C'); %3digits past decimal in time integer for n total
+%formatSpec = '#Code Version(%d) Inputs: Number on Neutrons(%d) Number of generations(%d)\n\r';
+%fprintf(formatSpec,A1,A2)
+fprintf(fileID,'%3f %0f\n',B'); %3digits past decimal in time integer for n total
 fclose(fileID); 
-%figure;
-%T=B(:,1);
-%N=A(:,2);
-%plot(T,N);
-%title('Neutrons over Time')
-%xlabel('Time in microseconds')
-%ylabel('Neutrons')
 B
 toc;
