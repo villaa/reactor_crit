9340b3766f21030c8a2409bf96ea0e728709ce81
rebekahmoline <47698863+rebekahmoline@users.noreply.github.com>
2019-03-05 20:18:38 -0700

    Working on changing the little thing we spoke of

--- matlab/NofTSimple.m
+++ matlab/NofTSimple.m
@@ -1,27 +1,28 @@
 %neutrons over time
 tic;
 ni = input('Enter initial number of neutrons: N(0) = ');
-g = input('Enter number of generations = ');
+%g = input('Enter number of generations = ');
 n = ni; %initially set total neutrons to initial number inputted
-t = 0; %initially time starts at zero
-out=[n,0];
-for ol = 1:g  
+t=0;
+dn=0;
+cf=0;
+out=zeros(0,2);
+for ol = 1:ni  
     nl = 0; %resetting these intermediate variables at the beginning of each iteration
-    cf = 0;
-    %tableA(tm,:)=[Neutrons Time]
-   for a = 1:n
-       x = rand();
-       y = rand();
+    %cf = 0;
+   for a=1:nt
+    x=rand();
+    y=rand();
         if (0.81 < x)    %if random # is greater than 0.81 it leaked
-            n = n - 1 ;   %If leaked total nuetrons-1
-            t(ol) = y * 200;
-       % elseif (0.81 >= x)     %0.81 probability for PNL
-       %    nl = nl + 1;  
+            dn = -1 ;   %If leaked total nuetrons-1
+            nt = nt + dn;
+            t = y * 200;
+        elseif (0.81 >= x)     %0.81 probability for PNL
+            t = y * 10;
+            dn = 0;
         end
-        next=[out, t];
-        out = [out;next];
-   %t(n)= y * 100;
-   end
+        out=[out;[t,dn]];
+        end    
   %  for b = 1:n %creating random numbers for f
   %      y = rand();
   %       if (0.72 < y)
@@ -40,5 +41,17 @@ for ol = 1:g
     %        en = 3;
     %    end
    % end 
+    if nt==0 
+        break;
+    end
 end
+B = sortrows (out, 1); %sort numerically by column one time     
+B= [[0,ni];B];
+B(:,2)=cumsum(B(:,2),1);     
+fileID = fopen('NofTData.txt','w');%write a a text file
+formatSpec = '#Code Version(5274) Inputs: Number on Neutrons(%d) Number of generations(%d)\n\r';
+fprintf(formatSpec,ni,g);
+fprintf(fileID,'%3f %0f\n',B'); %3digits past decimal in time integer for n total
+fclose(fileID); 
+B
 toc;
--- matlab/simplot.m
+++ matlab/simplot.m
@@ -1,12 +1,15 @@
-function simpleplot = simplot(B);
+function fig= simplot;
+fid=fopen('NofTData.txt');
+s=textscan(fid,'%d %d %d','headerlines',1);
+fclose(fid);
+X=s{1};
+Y=s{2};
 
 %This function plots the nx2 matrix B as(x,y) pairs
 
-X = B(:,1);
-Y = B(:,2);
 figure
 plot(X,Y);
-title('Neutrons over Time')
-xlabel('Time in microseconds')
-ylabel('Neutrons')
+title('Neutrons over Time');
+xlabel('Time in microseconds');
+ylabel('Neutrons');
 end
