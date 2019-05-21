09457dbaedaf59b0878e58efd5b71e2cb514dd2b
rebekahmoline <47698863+rebekahmoline@users.noreply.github.com>
2019-05-21 09:42:35 -0600

    5/21/19

--- matlab/LeakageDecay.m
+++ matlab/LeakageDecay.m
@@ -1,17 +1,17 @@
 %Leakage Only
 tic;
 ni = input('Enter initial number of neutrons: N(0) = ');
-g = input('Enter number of generations = ');
+FracLeak = input('Enter the fraction of leakage as a decimal = ');
 nt = ni; %initially set total neutrons to initial number inputted
 t=0;
 dn=0;
 cf=0;
 out=zeros(0,2);
-for z=1:g
+for z=1:ni
     for a=1:nt
     x=rand();
     y=rand();
-        if (0.81 < x)    %if random # is greater than 0.81 it leaked
+        if (FracLeak < x)    %if random # is greater than 0.81 it leaked
                 dn = -1 ;   %If leaked total nuetrons-1
                 nt = nt+dn;
                 t = (y * 200)+((z-1) * 200);
@@ -31,6 +31,8 @@ formatSpec = '#Code Version #### Inputs: Number on Neutrons(%d) Number of genera
 fprintf(formatSpec,ni,g);
 fprintf(fileID,'%3f %u\n',B'); %3digits past decimal in time integer for n total
 fclose(fileID); 
+fileID = fopen('dec5_10.txt','w');
+fprintf(fileID,'%6s %12s\n','N','FracLeak','fit parameter', 'error');
 
 B;
 toc;
--- matlab/simplot.m
+++ matlab/simplot.m
@@ -4,11 +4,14 @@ s=textscan(fid,'%f %f %f','headerlines',0);
 fclose(fid);
 X=s{1};
 Y=s{2}; %This function plots the nx2 matrix B as(x,y) pairs
-n0= s(1,2);
+n0= Y(1);
+%class(n0)
+%n0=double(n0);
 
-constant = lsqcurvefit(@f, [10,-1e-3], X, Y)
-xfit=0:10:n0;
+constant = lsqcurvefit(@f, [n0,-1e-3], X, Y)
+xfit=0:10:10000;
 yfit=f(constant,xfit); 
+
 
 figure
 hold on
@@ -18,4 +21,21 @@ plot(xfit, yfit, 'r', 'linewidth',2)
 title('Neutrons over Time');
 xlabel('Time in microseconds');
 ylabel('Neutrons');
+
+halflife= (ln(2))/(constant);
+matEr = {(yfit),(-(Y))};
+Erdiff = Sum(matEr, 2);
+Er = ((Erdiff)^2)/(sqrt(n0));
+ChiSqu = Sum(Er);
+EperDegfree = (ChiSqu)/2;
+
+datafile = fopen('dec5_10.txt','w+');
+fprintf(datafile,'%6s %12s\n','N','FracLeak','fit parameter','half-life', 'error');
+
+%datafile = fopen('dec5_10.txt','a');
+
+fmt = '%5d %f %f %f %f';
+fprintf(datafile,fmt,n0,FracLeak,constant,halflife,EperDegfree);
+
+
 end
